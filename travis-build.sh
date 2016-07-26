d76aa212425971ca698760598fee0dc6417a8f3d
Riley <rshanahan@SFOC02RR0TAG8WN.sea.corp.expecn.com>
2016-07-26 16:35:30 -0700

    deleted all files except for BugShaker

--- travis-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	./gradlew clean bugshaker:check example:assembleDebug
-else
-	./gradlew clean bugshaker:checkAndReport example:assembleDebug -PgitHubAuthToken="${PR_BOT_AUTH_TOKEN}" -PgitHubIssueNumber="${TRAVIS_PULL_REQUEST}"
-fi
